22c2447e5bee9a6d67963dd6a11f10ed3064d1e0
Ansh-verma21 <vermaansh13579@gmail.com>
2026-04-24 14:43:02 +0530

    initial commit

---
+++ src/main/java/com/example/testingMerge/TestingMergeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingMerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingMergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testingMerge/TestingMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingMerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
